4106e7b8508262f2538e799a21c486af022c5a88
Guilherme Almeida <guiallan.al@gmail.com>
2021-03-10 02:57:04 -0300

    chore(webpack): configura webpack source map
    Ref: C01M01A08

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
 export function App() {
+  // Erro lançado para validar no console do navegador
+  // o funcionamento do Source map (devtool)
+  throw new Error('Chama no Erro!!!');
+
   return <h1>Hello Guilherme!</h1>
 }
