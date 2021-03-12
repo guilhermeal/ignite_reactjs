d029fc6afe013a16433eea9274fc807b5f0995a2
Guilherme Almeida <guiallan.al@gmail.com>
2021-03-12 03:44:40 -0300

    chore: configura typescript
    Ref: C01M04A22

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -2,8 +2,19 @@ import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss'
 
+interface Repository {
+  name: string;
+  login: string;
+  description: string;
+  html_url: string;
+  owner: {
+    html_url: string;
+    login: string;
+  }
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(()=> {
     fetch('https://api.github.com/orgs/rocketseat/repos')
